9c0ff49712d30be6b248e4207c779da3039a373f
Akash Ranjan <114663040+akash769@users.noreply.github.com>
2024-01-29 12:41:15 +0530

    Update multi_google.js
    Add QScan Diagnostics
    
    Signed-off-by: Akash Ranjan <114663040+akash769@users.noreply.github.com>

--- platform/app/public/config/multi_google.js
+++ platform/app/public/config/multi_google.js
@@ -223,6 +223,29 @@ window.config = {
       },
     },
     {
+      namespace: '@ohif/extension-default.dataSourcesModule.dicomweb',
+      sourceName: 'qscan-diagnostics',
+      configuration: {
+        friendlyName: 'QScan Diagnostics',
+        name: 'GCP',
+        wadoUriRoot:
+          'https://healthcare.googleapis.com/v1/projects/prod-heme-platform/locations/asia-south1/datasets/fhir_prod_dataset/dicomStores/qscan-diagnostics/dicomWeb',
+        qidoRoot:
+          'https://healthcare.googleapis.com/v1/projects/prod-heme-platform/locations/asia-south1/datasets/fhir_prod_dataset/dicomStores/qscan-diagnostics/dicomWeb',
+        wadoRoot:
+          'https://healthcare.googleapis.com/v1/projects/prod-heme-platform/locations/asia-south1/datasets/fhir_prod_dataset/dicomStores/qscan-diagnostics/dicomWeb',
+        qidoSupportsIncludeField: true,
+        imageRendering: 'wadors',
+        thumbnailRendering: 'wadors',
+        enableStudyLazyLoad: true,
+        supportsFuzzyMatching: true,
+        supportsWildcard: false,
+        dicomUploadEnabled: true,
+        omitQuotationForMultipartRequest: true,
+        configurationAPI: 'ohif.dataSourceConfigurationAPI.google',
+      },
+    },
+    {
       namespace: '@ohif/extension-default.dataSourcesModule.dicomwebproxy',
       sourceName: 'dicomwebproxy',
       configuration: {
