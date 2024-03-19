57aa7560ef1416803a2befcd0df1d85fc958892a
Akash Ranjan <114663040+akash769@users.noreply.github.com>
2024-03-19 09:52:40 +0530

    Update multi_google.js
    Add mittal-imaging
    
    Signed-off-by: Akash Ranjan <114663040+akash769@users.noreply.github.com>

--- platform/app/public/config/multi_google.js
+++ platform/app/public/config/multi_google.js
@@ -315,6 +315,29 @@ window.config = {
       },
     },
     {
+      namespace: '@ohif/extension-default.dataSourcesModule.dicomweb',
+      sourceName: 'mittal-imaging',
+      configuration: {
+        friendlyName: 'Mittal Imaging Centre',
+        name: 'GCP',
+        wadoUriRoot:
+          'https://healthcare.googleapis.com/v1/projects/prod-heme-platform/locations/asia-south1/datasets/fhir_prod_dataset/dicomStores/mittal-imaging/dicomWeb',
+        qidoRoot:
+          'https://healthcare.googleapis.com/v1/projects/prod-heme-platform/locations/asia-south1/datasets/fhir_prod_dataset/dicomStores/mittal-imaging/dicomWeb',
+        wadoRoot:
+          'https://healthcare.googleapis.com/v1/projects/prod-heme-platform/locations/asia-south1/datasets/fhir_prod_dataset/dicomStores/mittal-imaging/dicomWeb',
+        qidoSupportsIncludeField: true,
+        imageRendering: 'wadors',
+        thumbnailRendering: 'wadors',
+        enableStudyLazyLoad: true,
+        supportsFuzzyMatching: true,
+        supportsWildcard: false,
+        dicomUploadEnabled: true,
+        omitQuotationForMultipartRequest: true,
+        configurationAPI: 'ohif.dataSourceConfigurationAPI.google',
+      },
+    },
+    {
       namespace: '@ohif/extension-default.dataSourcesModule.dicomwebproxy',
       sourceName: 'dicomwebproxy',
       configuration: {
